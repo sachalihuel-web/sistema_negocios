61b79dcbf753e8d372962d9074921b17df17e092
Sacha Zozaya <sachalihuel@gmail.com>
2026-05-09 02:28:41 -0400

    Adapta login para Streamlit Cloud

--- src/auth.py
+++ src/auth.py
@@ -1,18 +1,30 @@
 import os
-
+import streamlit as st
 from dotenv.main import load_dotenv
 
 load_dotenv()
 
-USUARIO = os.getenv("APP_USER")
 
-PASSWORD = os.getenv("APP_PASSWORD")
+def obtener_credenciales():
+
+    usuario = st.secrets.get(
+        "APP_USER",
+        os.getenv("APP_USER")
+    )
+
+    password = st.secrets.get(
+        "APP_PASSWORD",
+        os.getenv("APP_PASSWORD")
+    )
+
+    return usuario, password
 
 
 def login(usuario, password):
 
-    if usuario == USUARIO and password == PASSWORD:
+    usuario_correcto, password_correcto = obtener_credenciales()
 
+    if usuario == usuario_correcto and password == password_correcto:
         return True
 
     return False
